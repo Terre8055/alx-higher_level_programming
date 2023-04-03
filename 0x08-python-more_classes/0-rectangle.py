f467b9879396018f9dbec46c5865e620c711c976
Terre8055 <michaelappiah2018@icloud.com>
2023-04-03 23:23:47 +0000

    Refactor

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ module that created a rectangle class"""
 
-class Rectangle:
-    """
-    A class that defines a rectangle.
-    """
-    pass
 
+class Rectangle:
+    """  pass """
+    pass
 
