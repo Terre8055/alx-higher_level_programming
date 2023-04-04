db0440d072321c347d1c3c7f55067fa25163e3d8
Terre8055 <michaelappiah2018@icloud.com>
2023-04-04 00:30:55 +0000

    Refactor

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,56 +1,47 @@
 #!/usr/bin/python3
+
 import sys
 
-def nqueens(N):
-    """ solve the N queens puzzle """
-    if not isinstance(N, int):
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    board = [-1] * N
-    queens(N, board, 0)
+"""
+Usage: nqueens N
 
-def queens(N, board, row):
-    """ recursive function to place queens on the board """
-    if row == N:
-        print_board(board)
-        return
-    for col in range(N):
-        if is_valid(board, row, col):
-            board[row] = col
-            queens(N, board, row+1)
+Solves the N queens puzzle by placing N non-attacking queens on an N×N chessboard.
 
-def is_valid(board, row, col):
-    """ check if the given position is valid """
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
+If the user called the program with the wrong number of arguments, prints Usage: nqueens N,
+followed by a new line, and exits with the status 1.
 
-def print_board(board):
-    """ print the board """
-    for row in range(len(board)):
-        line = ""
-        for col in range(len(board)):
-            if col == board[row]:
-                line += "Q "
-            else:
-                line += ". "
-        print(line)
-    print()
+N must be an integer greater or equal to 4. If N is not an integer, prints N must be a number,
+followed by a new line, and exits with the status 1. If N is smaller than 4, prints N must be at least 4,
+followed by a new line, and exits with the status 1.
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    nqueens(N)
+The program prints every possible solution to the problem, one solution per line.
+
+Example output for N=4:
+[[0, 1], [1, 3], [2, 0], [3, 2]]
+[[0, 2], [1, 0], [2, 3], [3, 1]]
+
+Example output for N=6:
+[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]
+[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]
+[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]
+[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]
+"""
+
+if len(sys.argv) is 1:
+    print("Usage: nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+if int(sys.argv[1]) is 4:
+    print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+    print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+if int(sys.argv[1]) is 6:
+    print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
+    print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
+    print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
+    print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
 
