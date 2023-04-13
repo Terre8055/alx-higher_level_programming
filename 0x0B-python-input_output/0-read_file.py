fa9a9bf430ebc25ae32035dc0247c863708fb5a3
Terre8055 <michaelappiah2018@icloud.com>
2023-04-13 22:14:32 +0000

    Set mode for file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,12 +3,7 @@
 
 
 def read_file(filename=""):
-        '''
-        function to read the contents of a file
-        @r - read
-        @filename - relative path to file
-        @encoding - parse file in utf-8 encoding
-        '''
+    """Read a text file and output its contents to stdout"""
 
-        with open(filename, mode='r', encoding='utf-8') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         print(f.read(), end="")
