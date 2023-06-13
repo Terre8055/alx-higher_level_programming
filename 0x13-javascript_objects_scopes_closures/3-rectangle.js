ddde6c2eaebb58bc9cb4d11a42df0360a78bda60
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-06-13 22:44:26 +0000

    JS classes

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 0 || h < 0 || w == 0 || h == 0) {
+      return null
+    } else {
+      this.width = w
+      this.height = h
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = ''
+      for (let j = 0; j < this.width; j++) {
+        line += 'x'
+      }
+      console.log(line)
+    }
+  }
+}
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 0 || h < 0 || w == 0 || h == 0) {
+      return null
+    } else {
+      this.width = w
+      this.height = h
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = ''
+      for (let j = 0; j < this.width; j++) {
+        line += 'x'
+      }
+      console.log(line)
+    }
+  }
+
+  rotate () {
+    const temp = this.width
+    this.width = this.height
+    this.height = temp
+  }
+
+  double () {
+    this.width *= 2
+    this.height *= 2
+  }
+}
