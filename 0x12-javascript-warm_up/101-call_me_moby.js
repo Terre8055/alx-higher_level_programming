32e452cf4ca96d54261a7344c7059142754f7231
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-06-13 11:54:54 +0000

    Pass linter on semicolons

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,10 +2,10 @@
 
 exports.callMeMoby = (x, theFunction) => {
   if (x) {
-    let start = 0
+    let start = 0;
     while (start < x) {
-      theFunction()
-      start += 1
+      theFunction();
+      start += 1;
     }
   }
-}
+};
